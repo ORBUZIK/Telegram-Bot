1b73a0631a7ef0bb4f1f0342a4713108f7eaf5f4
ORBUZIK <66378878+ORBUZIK@users.noreply.github.com>
2024-06-28 22:56:46 +0300

    ...

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-access_key = 'f8730de31af030f5526b102c0ad02a9c'
-TOKEN = "5239808309:AAETd944x9jm7Iwlh0og4Tspjm6LaKwL5lc"
+access_key = 'KEY'
+TOKEN = "TOKEN"
 
 available_dict = {
     'рубль': 'RUB',
